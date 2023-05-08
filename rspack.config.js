5534728b1ce5fa67aee319ed8473a7cd0036ac22
Nikolay Cherednik <32453685+cherrynik@users.noreply.github.com>
2023-05-08 13:31:25 +0400

    chore: config changes

--- rspack.config.js
+++ rspack.config.js
@@ -1,27 +1,39 @@
+/**
+ * @type {import('@rspack/core').Configuration}
+ */
 const path = require('path');
 const { composePlugins, withNx, withWeb } = require('@nx/rspack');
 
 module.exports = composePlugins(withNx({}), withWeb(), (config) => {
-  config.module = {
-    rules: [
-      {
-        test: /\.(png|gif)$/,
-        type: 'asset/resource',
+  config = {
+    ...config,
+    module: {
+      rules: [
+        {
+          test: /\.(png|gif)$/,
+          type: 'asset',
+        },
+        {
+          test: /\.(eot|ttf|otf|woff|woff2)$/,
+          type: 'asset',
+        },
+        {
+          test: /\.svg$/,
+          type: 'asset',
+        },
+      ],
+    },
+    resolve: {
+      alias: {
+        '@': path.resolve(__dirname, './src'),
       },
-      {
-        test: /\.(eot|ttf|otf|woff|woff2)$/,
-        type: 'asset/resource',
-      },
-      {
-        test: /\.svg$/,
-        type: 'asset/inline',
-      },
-    ],
-  };
-  config.resolve = {
-    alias: {
-      '@': path.resolve(__dirname, './src'),
+    },
+    optimization: {
+      sideEffects: false,
+      moduleIds: 'named',
+      minimize: true,
     },
   };
+
   return config;
 });
